4c3ea4de7f1492c71103c647070442d89bf46fce
Gyesik Lee <gslee@hknu.ac.kr>
2022-07-15 17:43:46 +0900

    ch01 being prepared

--- jupyter-book/_build/jupyter_execute/intro.py
+++ jupyter-book/_build/jupyter_execute/intro.py
@@ -1,49 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # 강의 소개
-
+# **소개**
+# 
 # 머신러닝/딥러닝 기술이 획기적으로 발전하면서 데이터 분석 및 인공지능 관련 연구의 
-# 중요성이 사회, 경제, 산업의 거의 모든 분야에 지대한 영향을 미치고 있으며,
-# 앞으로 그런 경향이 더욱 강화될 것으로 기대된다.
+# 중요성이 자율주행, 의료영상, 농업, 교육, 재난 예방, 제조업 등 
+# 사회, 경제, 산업의 거의 모든 분야에 지대한 영향을 미치고 있다.
+# 이런 경향이 앞으로 더욱 강화될 것으로 기대되며 약간의 코딩 지식을 가진
+# 어느 누구나 데이터로부터 학습하는 스마트 앱을 개발 할 수 있을 정도로
+# 딥러닝 기술이 누구나 쉽게 활용할 수 있을 정도로 대중화될 것이다.
 # 
-# 본 강의는 머신러닝/딥러닝 기본 아이디어와 다양한 활용법을 실전 예제와 함께 전달한다. 
-# 머신러닝/딥러닝 학습을 위해 아래 분야의 기초지식이 요구되지만 여기서는 필요한 최소의 내용만 다룬다.
-# 
-# - 파이썬 프로그래밍
-# - 미적분학, 선형대수, 확률과통계
-
-# **데이터 과학, 인공지능, 머신러닝, 딥러닝**
-
-# 아래 그림은 인공지능, 머신러닝, 딥러닝, 데이터 과학 네 분야의 관계를 설명한다.
-
-# <div align="center"><img src="https://github.com/codingalzi/handson-ml3/blob/master/jupyter-book/imgs/intro/ai-ml-relation.png?raw=true" style="width:500px;"></div>
-# 
-# <p style="text-align: center;">
-#     그림 출처: <a href="http://www.kyobobook.co.kr/readIT/readITColumnView.laf?thmId=00198&sntnId=14142">교보문고: 에이지 오브 머신러닝</a>
-# </p>
-
-# * 데이터 과학: 주어진 데이터로부터 수학과 통계 지식을 활용하여 필요한 정보를 추출하는 학문 분야
-# * 인공지능: 사고(thinking), 학습(learning) 등 인간의 지적능력을 컴퓨터를 통해 구현하는 
-#     기술 또는 해당 연구 분야
-# * 머신러닝: 컴퓨터가 데이터로부터 스스로 정보를 추출하는 기법 또는 해당 연구 분야.
-# * 딥러닝: 심층 신경망 이론을 기반으로 복잡한 비선형 문제를 해결하는 머신러닝 기법 
-#     또는 해당 연구 분야    
-
-# 역사적 관점에서 바라본 인공지능, 머신러닝, 딥러닝의 관계는 다음과 같다.
-# 
-# <div align="center"><img src="https://github.com/codingalzi/handson-ml3/blob/master/jupyter-book/imgs/intro/ai-ml-relation2.png?raw=true" style="width:600px;"></div>
+# 여기서는 딥러닝의 기본 개념과 함께 다양한 딥러닝 기법을 최대한 직관적으로 전달하는 일에
+# 집중하며, 이를 위해 다양한 예제와 적절한 코드를 이용한다.
+# 예제에 사용되는 코드는 텐서플로우<font size='2'>TensorFlow</font> 2와
+# 케라스<font size='2'>Keras</font>를 파이썬 딥러닝 프레임워크로 사용한다.
 # 
 # 
-# <p style="text-align: center;">
-#     그림 출처: <a href="https://blogs.nvidia.com/blog/2016/07/29/whats-difference-artificial-intelligence-machine-learning-deep-learning-ai/">NVIDIA 블로그</a>
-# </p>
 
 # **주요 내용**
 # 
-# 머신러닝의 주요 개념과 알고리즘을 소개한다.
-# 여기서 소개하는 내용은 오렐리앙 제롱의 핸즈온 머신러닝 (3판)의 1부 내용을 기본 참고서로 사용한다.
-
-# **목차**
+# 여기서 소개하는 내용은 프랑소와 숄레의 
+# [Deep Learning with Python(2판)](https://www.manning.com/books/deep-learning-with-python-second-edition)의
+# [주피터 노트북](https://github.com/fchollet/deep-learning-with-python-notebooks) 내용을 
+# 기본 참고서로 사용하며 아래 내용을 전달한다.
 # 
-# 1. {ref}`ch:building_blocks`
+# - 딥러닝 개념
+# - 딥러닝 활용
+#     - 컴퓨터비전: 이미지 분석 및 분할
+#     - 자연어처리: 시계열 예측, 텍스트 분류, 자동 번역, 텍스트 생성
+
+# **전제 사항**
+# 
+# 코드 이해를 위해 파이썬 프로그래밍과 넘파이<font size='2'>NumPy</font>에 대한 
+# 기초 이상의 지식이 요구된다.
